9b1ee0ec4bfe9ff3f90d6767d4ac263b83ada43d
dindincow <dindincow@gmail.com>
2022-03-31 22:22:05 +0800

    i18n

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="navbar">
+    <Hamburger />
+    <breadcrumb class="breadcrumb-container" />
     <div class="right-menu">
       <!-- 头像 -->
       <el-dropdown class="avatar-container" trigger="click">
@@ -31,6 +33,8 @@
 
 <script setup>
 import { useStore } from 'vuex'
+import Hamburger from '@/components/hamburger'
+import breadcrumb from '@/components/Breadcrumb'
 
 const store = useStore()
 const logout = () => {
@@ -45,6 +49,22 @@ const logout = () => {
   position: relative;
   background: #fff;
   box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+
+  .breadcrumb-container {
+    float: left;
+  }
+  .hamburger-container {
+    line-height: 46px;
+    height: 100%;
+    float: left;
+    cursor: pointer;
+    // hover 动画
+    transition: background 0.5s;
+
+    &:hover {
+      background: rgba(0, 0, 0, 0.1);
+    }
+  }
 
   .right-menu {
     display: flex;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
+// i18n （PS：导入放到 APP.vue 导入之前，因为后面我们会在 app.vue 中使用国际化内容）
+import i18n from '@/i18n'
 
 // 全局樣式
 import './styles/index.scss'
@@ -16,4 +18,4 @@ import './permission'
 const app = createApp(App)
 installIcons(app)
 installElementPlus(app)
-app.use(store).use(router).mount('#app')
+app.use(store).use(router).use(i18n).mount('#app')
